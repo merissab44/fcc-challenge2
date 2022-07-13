f120246acb0d8475db124de4e3eae426b6c121b9
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2022-07-13 04:25:56 +0000

    added json route

--- myApp.js
+++ myApp.js
@@ -4,7 +4,12 @@ let app = express();
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/', function(req,res){
   res.sendFile('views/index.html' , { root : __dirname})
+})
 
+app.get('/json', (req, res) => {
+  res.json({
+    message: "Hello json"
+  })
 })
 
 
