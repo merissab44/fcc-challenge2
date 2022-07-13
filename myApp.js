3a773d2764e6a6c2b057bdd1e60e7313891fd6ea
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2022-07-13 05:00:24 +0000

    added spacing to format

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 
 app.use((req, res, next) => {
-  console.log(req.method + " " + req.path + "-" +     req.ip)
+  console.log(req.method + " " + req.path + " " + "- " +  req.ip)
   next()
 })
 
