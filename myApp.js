f4537b466db9b780750f784d43781caa506221ed
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2022-07-13 04:57:58 +0000

    created simple logger

--- myApp.js
+++ myApp.js
@@ -2,23 +2,28 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + "-" +     req.ip)
+  next()
+})
+
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/', function(req,res){
   res.sendFile('views/index.html' , { root : __dirname})
 })
 
-app.get('/json', (req, res) => {
-  const secret = process.env['MESSAGE_STYLE']
-  if (secret === "uppercase") {
-    res.json({
-      message: "Hello json".toUpperCase()
-    })
-  } else {
-     res.json({
-      message: "Hello json"
-    })
-  }
-})
+// app.get('/json', (req, res) => {
+//   const secret = process.env['MESSAGE_STYLE']
+//   if (secret === "uppercase") {
+//     res.json({
+//       message: "Hello json".toUpperCase()
+//     })
+//   } else {
+//      res.json({
+//       message: "Hello json"
+//     })
+//   }
+// })
 
 
 
